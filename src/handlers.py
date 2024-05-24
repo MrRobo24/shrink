258b5ba5b96f7949902753bcd802237ed3ea7df0
MrRobo24 <sumanarpit.24@gmail.com>
2024-05-25 04:18:25 +0530

    add url validator

--- src/handlers.py
+++ src/handlers.py
@@ -1,14 +1,17 @@
-from flask import redirect
+from flask import redirect, abort
 from models import URL_Map
-from utils import generate_short_url, incr_suff_on_collision
+from utils import generate_short_url, incr_suff_on_collision, validate_long_url
 from database import insert_db, find_one_db
   
 class Handler():
   def handleLoadCall(self, short_url):
     url_map = find_one_db(URL_Map, short_url)
-    return redirect("http://" + url_map.long_url) if url_map != None else "URL fetch failed"
+    return redirect("http://" + url_map.long_url) if url_map != None else abort(400, "URL fetch failed")
     
   def handleShrinkCall(self, long_url):
+    if not validate_long_url("https://" + long_url):
+      return abort(400, "Invalid URL provided")  
+      
     short_url = generate_short_url(long_url)
     old_url_map: URL_Map = find_one_db(URL_Map, short_url)
     if old_url_map != None:
@@ -18,4 +21,4 @@ class Handler():
         short_url = incr_suff_on_collision(short_url)
     else:  
       url_map = URL_Map(long_url=long_url, short_url=short_url)
-      return "URL insertion failed" if not insert_db(url_map) else short_url
+      return abort(500, "URL insertion failed") if not insert_db(url_map) else short_url
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 import hashlib
 import base64
+import validators
 
 def generate_short_url(long_url):
     # Create a SHA-256 hash of the long URL
@@ -12,3 +13,10 @@ def generate_short_url(long_url):
 def incr_suff_on_collision(short_url):
     print("handling collision for:", short_url)
     return short_url[:-1] + str(int(short_url[-1]) + 1)
+
+def validate_long_url(long_url):
+    try:
+        return validators.url(long_url)
+    except Exception as e:
+        print(e)
+        return False
