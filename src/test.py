7bdc5f6900c57a8953b924d724e2b2ae0eaa5bfb
MrRobo24 <sumanarpit.24@gmail.com>
2024-05-27 01:35:37 +0530

    add more test

--- src/test.py
+++ src/test.py
@@ -1,5 +1,4 @@
 import unittest
-import json
 from server import app, start_app
 
 class Tests(unittest.TestCase):
@@ -9,18 +8,28 @@ class Tests(unittest.TestCase):
     start_app()
     cls.client = app.test_client()
   
-  def test_shrink(self):
+  def test_shrink_inserted(self):
     long_url = "www.linkedin.com"
     response = self.client.post("/shrink/" + long_url)
     self.assertEqual(response.status_code, 200)
-    self.assertEqual(response.text, "gj-1sp-0")
     
-  def test_load(self):
-    short_url = "gj-1sp-"
+  def test_shrink_invalid_url(self):
+    long_url = "123@.linkedin.com"
+    response = self.client.post("/shrink/" + long_url)
+    self.assertEqual(response.status_code, 400)
+    
+  def test_load_fetched(self):
+    self.test_shrink_inserted()
+    short_url = "gj-1sp-0"
     response = self.client.get("/" + short_url)
     self.assertEqual(response.status_code, 302)
     self.assertIn("Location", response.headers)
     self.assertEqual(response.headers["Location"], "http://www.linkedin.com")
+  
+  def test_load_not_found(self):
+    short_url = "random-0"
+    response = self.client.get("/" + short_url)
+    self.assertEqual(response.status_code, 400)
     
   @classmethod
   def tearDownClass(cls):
